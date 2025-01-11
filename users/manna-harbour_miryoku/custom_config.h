4c3a711a33c5a15573d7616cf1ca0be75ecec576
anjerukare <anjerukare@gmail.com>
2025-01-11 16:28:30 +0700

    Replace left hand bottom row to match ANSI Colemak DH layout

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -20,7 +20,7 @@ U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_SPC),  LT(U
 #define MIRYOKU_LAYER_TAP \
 KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_SCLN,           \
 KC_A,              KC_R,              KC_S,              KC_T,              KC_G,              KC_M,              KC_N,              KC_E,              KC_I,              KC_O,              \
-KC_Z,              KC_X,              KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           KC_DOT,            KC_SLSH,           \
+KC_X,              KC_C,              KC_D,              KC_V,              KC_Z,              KC_K,              KC_H,              KC_COMM,           KC_DOT,            KC_SLSH,           \
 U_NP,              U_NP,              KC_ESC,            KC_SPC,            KC_TAB,            KC_ENT,            TO(U_BASE),        KC_DEL,            U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
